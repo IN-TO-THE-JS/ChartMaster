5aae51178a39cc2254bf611b9a0a461d22bb7c25
minji <dylk97@gmail.com>
2023-05-20 21:40:24 +0900

    feat: change dount, bar chart

--- pages/chart/index.tsx
+++ pages/chart/index.tsx
@@ -1,44 +1,44 @@
 import React, { useEffect, useState } from "react";
-import BarChart from "./BarChart";
-import DonutChart from "./DonutChart";
+import BarChart from "./Bar1";
+import DonutChart from "./Donut1";
 // import { BarChart1, BarChart2, BarChart3 } from "./TestBarChart";
 
 const datas = [
-  [10, 30, 40, 20],
-  [10, 40, 30, 20, 50, 10],
-  [60, 30, 40, 20, 30],
+    [10, 30, 40, 20],
+    [10, 40, 30, 20, 50, 10],
+    [60, 30, 40, 20, 30],
 ];
 var i = 0;
 
 const Chart = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    changeData();
-  }, []);
+    useEffect(() => {
+        changeData();
+    }, []);
 
-  const changeData = () => {
-    setData(datas[i++]);
-    if (i === datas.length) i = 0;
-  };
+    const changeData = () => {
+        setData(datas[i++]);
+        if (i === datas.length) i = 0;
+    };
 
-  // const barData = [
-  //   { name: "A", value: 10 },
-  //   { name: "B", value: 20 },
-  //   { name: "C", value: 15 },
-  //   { name: "D", value: 30 },
-  //   { name: "E", value: 25 },
-  // ];
-  return (
-    <div>
-      <button onClick={changeData}>Change Data</button>
-      {/* <BarChart1 />
+    // const barData = [
+    //   { name: "A", value: 10 },
+    //   { name: "B", value: 20 },
+    //   { name: "C", value: 15 },
+    //   { name: "D", value: 30 },
+    //   { name: "E", value: 25 },
+    // ];
+    return (
+        <div>
+            <button onClick={changeData}>Change Data</button>
+            {/* <BarChart1 />
       <BarChart3 data={barData} /> */}
-      {/* <BarChart2 width={600} height={400} data={data} /> */}
-      <BarChart width={600} height={400} data={data} />
-      <DonutChart width={600} height={400} data={data} />
-    </div>
-  );
+            {/* <BarChart2 width={600} height={400} data={data} /> */}
+            <BarChart width={600} height={400} data={data} />
+            <DonutChart width={600} height={400} data={data} />
+        </div>
+    );
 };
 
 export default Chart;
